499169bffaf432a48a585de7c4987c77e91bc0f0
petripykalainen <petri.pykalainen@bastu.net>
2018-11-08 10:53:24 +0200

    Created movement and jump

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    Vector3 offset;
+    Transform player;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player").transform.GetChild(0);
+        Debug.Log("Found " + player);
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = player.transform.position + offset;
+	}
+}
