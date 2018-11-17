0c26d4a406833754fd0ee4046209ca43fa6ac916
Toosmo <toosmo@users.noreply.github.com>
2018-11-17 19:24:37 +0200

    Make the health system more versatile

---
+++ Assets/Script/PlayerAttack.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    GameObject enemy;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+
+    bool enemyInRange;
+    float timer;
+
+
+    void Start()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        playerHealth = GetComponent<PlayerHealth>();
+        enemyHealth = enemy.GetComponent<EnemyHealth>();
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == enemy)
+        {
+            enemyInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == enemy)
+        {
+            enemyInRange = false;
+        }
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && enemyInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+    }
+
+
+    void Attack()
+    {
+        timer = 0f;
+
+        if (enemyHealth.currentHealth > 0 && !playerHealth.isDead)
+        {
+            enemyHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public Text deathMessage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    public bool isDead;
+
+    bool damaged;
+
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+        deathMessage.text = "";
+    }
+
+
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        // FIXME: Slider for some reason doesn't go to zero.
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+        deathMessage.text = "You have died!";
+    }
+}
