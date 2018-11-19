bcfa0686eb6df4475f6c0ddbc209363840427b89
Toosmo <toosmo@users.noreply.github.com>
2018-11-19 12:38:49 +0200

    Stop the MinimapCamera from falling down whenever the player falls down

---
+++ Assets/Script/MinimapCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x + offset.x, transform.position.y, player.transform.position.z + offset.z);
+    }
+}
