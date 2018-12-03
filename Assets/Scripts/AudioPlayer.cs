7f059e37c340167feca79f97dccb6895ff49e481
Toosmo <Toosmo@users.noreply.github.com>
2018-12-03 10:14:00 +0200

    Inventory

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+    private Inventory inventory;
+    public AudioClip pickupAudio;
+    public AudioClip inventoryAudio;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playInventoryAudio()
+    {
+        source.PlayOneShot(inventoryAudio, 1f);
+    }
+    public void playPickupAudio()
+    {
+        source.PlayOneShot(pickupAudio, 1f);
+    }
+}
