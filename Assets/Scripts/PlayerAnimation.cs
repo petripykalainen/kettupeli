731f8d3ff9eb4211826307b1116c0b3b7664c1c8
jmKzz <juhamatti1501@student.hamk.fi>
2018-12-03 12:21:43 +0200

    Player animations v1
    paska näätä

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    private float move_x;
+    private float move_y;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*if (Input.GetKeyDown(""))
+        {
+            anim.Play("move", -1, 1.0f);
+        }*/
+
+        move_x = Input.GetAxis("Horizontal");
+        move_y = Input.GetAxis("Vertical");
+        anim.SetFloat("move_x", move_x);
+        anim.SetFloat("move_y", move_y);
+    }
+}
