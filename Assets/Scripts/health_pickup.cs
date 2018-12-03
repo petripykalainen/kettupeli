0e555873bd1effd5772bb9ac7d73e057e04475d8
jmKzz <juhamatti1501@student.hamk.fi>
2018-12-03 13:37:06 +0200

    Added an effect for item use
    toisinsanoen, täyttä paskaa

---
+++ Assets/Scripts/health_pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health_pickup : MonoBehaviour {
+
+    private Transform player;
+    public GameObject healthEffect;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Use()
+    {
+        Instantiate(healthEffect, player.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
