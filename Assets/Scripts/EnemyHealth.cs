2656e257320733b6c5cba9e7eae06fcd7a1b749e
petripykalainen <petri.pykalainen@bastu.net>
2018-12-03 11:01:27 +0200

    Added rolling ball trap, TODO damage

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -39,16 +39,14 @@ public class EnemyHealth : MonoBehaviour
 
     public void Death()
     {
-        // TODO: Death animation, or at least stop the enemy from chasing the player.
         anim.SetBool("IsDead", isDead);
-        anim.SetTrigger("Die");
-        deathDelay = deathDelay + anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
         StartCoroutine(RemoveBody());
     }
 
     IEnumerator RemoveBody()
     {
-        Debug.Log("You have slaughtered the enemy!");
+        anim.SetTrigger("Die");
+        deathDelay = deathDelay + anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
         Debug.Log(anim.GetCurrentAnimatorClipInfo(0)[0].clip.length);
         Debug.Log("Removing body in " + deathDelay + " seconds");
         yield return new WaitForSeconds(deathDelay);
