5b7f7180e6728b5f5766810fe5cf8bbb7a05b193
petripykalainen <petri.pykalainen@bastu.net>
2018-12-03 13:37:54 +0200

    Started with enemyspawner

---
+++ Assets/Scripts/EneymSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EneymSpawner : MonoBehaviour {
+
+    public Enemy enemyprefab;
+    public int enemyAmount = 3;
+    public float spawnTime = 3f;
+    public bool spawnComplete = false;
+    public float timer;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        bool timeToSpawnEnemies = timer > spawnTime;
+        if (timeToSpawnEnemies)
+        {
+            SpawnWave();
+            timer = 0f;
+        }
+	}
+
+    private void SpawnWave()
+    {
+        for (int i = 0; i < enemyAmount; i++)
+        {
+            StartCoroutine(CreateSingleEnemy());
+        }
+        spawnComplete = true;
+    }
+
+    IEnumerator CreateSingleEnemy()
+    { 
+        Debug.Log("Creating enemy");
+        yield return new WaitForSeconds(spawnTime);
+        Debug.Log("Enemy Created");
+        GameObject.Instantiate(enemyprefab, transform.position, Quaternion.identity);
+    }
+}
